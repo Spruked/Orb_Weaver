6e5ed0bb45ce434d742c1523cc98d7fedfe09ae6
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-08-14 13:59:19 -0500

    Keep Website ORB mounted across public route changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AutonomousOrb from './landing/AutonomousOrb';
 import { installWarmArrivalAudioPolicy } from './landing/orbArrivalAudioPolicy';
@@ -12,9 +13,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
       <AutonomousOrb size={156} />
-    </>
+    </BrowserRouter>
   </React.StrictMode>
 );
