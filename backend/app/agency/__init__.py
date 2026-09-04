f290eeb9def65c94c3646c9b80682e1df7088522
Bryan Anthony Spruk <bryanspruk7@outlook.com>
2026-09-04 01:14:33 -0500

    Add agency runtime package

---
+++ backend/app/agency/__init__.py
@@ -0,0 +1,5 @@
+"""Shared Orb Weaver agency-contract runtime helpers."""
+
+from .contract import agency_contract_status, load_primitive_registry
+
+__all__ = ["agency_contract_status", "load_primitive_registry"]
